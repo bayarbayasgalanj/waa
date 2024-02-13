bfdcdd0235223f490d95b4d45bb753174186cb44
Bayasaa <bayarbayasgalan.j@gmail.com>
2024-02-13 17:18:50 -0600

    done part two

--- Lab1/Lab1SpringMVC/src/main/java/mvc/HelloController.java
+++ Lab1/Lab1SpringMVC/src/main/java/mvc/HelloController.java
@@ -9,15 +9,41 @@ import java.util.Map;
 @Controller
 public class HelloController {
     @RequestMapping("/hello")
-    public ModelAndView hello( @RequestParam(value="firstname") String firstName,
-                        @RequestParam(value="lastname") String lastName) {
+    public ModelAndView hello(@RequestParam(value = "firstname") String firstName,
+            @RequestParam(value = "lastname") String lastName) {
 
-        String message=firstName+" "+lastName;
+        String message = firstName + " " + lastName;
 
         Map<String, Object> params = new HashMap<>();
         params.put("message", message);
+        System.out.println(message);
+        return new ModelAndView("hello", params);
+    }
 
-        return new ModelAndView("hello",params);
+    @RequestMapping("/calc")
+    public ModelAndView calc(@RequestParam(value = "num1") String num1,
+            @RequestParam(value = "num2") String num2,
+            @RequestParam(value = "op") String op) {
+        double result = calculate(op, Integer.parseInt(num1), Integer.parseInt(num2));
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("num1", "Number 1 = " + num1);
+        params.put("op", "Operator = " + op);
+        params.put("num2", "Number 2 = " + num2);
+        params.put("result", "Result = " + result);
+        return new ModelAndView("calc", params);
+    }
+
+    private double calculate(String operator, int x, int y) {
+        if (operator.equals("+")) {
+            return x + y;
+        } else if (operator.equals("-")) {
+            return x - y;
+        } else if (operator.equals("*")) {
+            return x * y;
+        } else if (operator.equals("/")) {
+            return x / y;
+        }
+        return 0;
     }
 }
-
